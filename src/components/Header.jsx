84bf71a182439c526fc687ec1e769696a5d0e518
Nogaye Ndao <nogayendao2020@gmail.com>
2025-12-08 21:37:13 -1200

    notification

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -161,9 +161,9 @@ export default function Header({ onOpenSidebar }) {
           {/* Notifications */}
           <button className="relative p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-700 transition">
             <BellIcon className="w-5 h-5 text-gray-700 dark:text-gray-200" />
-            <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full px-1.5">
+            {/* <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full px-1.5">
               2
-            </span>
+            </span> */}
           </button>
 
           {/* Profile Dropdown */}
