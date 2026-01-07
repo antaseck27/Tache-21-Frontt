7189ef117b59704b3474d5a52a2e71c0789526c7
anta seck <antalissa10@gmail.com>
2026-01-07 04:01:56 +0000

    mode nocture

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -107,7 +107,7 @@ export default function Transactions() {
         </p>
 
         {/* Résumé */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 mb-6 card">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 mb-6 ">
           <SummaryCard title="Total Transactions" value={filteredTransactions.length} />
           <SummaryCard
             title="Total Revenus"
